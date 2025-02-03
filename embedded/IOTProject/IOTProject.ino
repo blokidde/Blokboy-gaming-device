2e866b73ba6dbf8cea83d4511196a34996c9b243
egmondj10 <jurriaan.egmond@hva.nl>
2025-02-03 11:53:08 +0100

    added initial arduino ide file for the iotproject

---
+++ embedded/IOTProject/IOTProject.ino
@@ -0,0 +1,13 @@
+/*
+ * Description: IOTProject based on the handheld gamedevice blueprint.
+ * Author:  Jurriaan Egmond
+ * Date:    03/02/2025
+ */
+
+#include <wire.h>
+
+void setup() {
+}
+
+void loop() {
+}
