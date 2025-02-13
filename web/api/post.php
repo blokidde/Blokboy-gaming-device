f320d9a4d390841b7c42a528649b1a45311743a7
egmondj10 <jurriaan.egmond@hva.nl>
2025-02-13 23:08:54 +0100

    added debugging text for api and added cleaner syntax

--- web/api/post.php
+++ web/api/post.php
@@ -10,13 +10,28 @@ if($data){
     $totalright = $data['totalright'];
     $totalleft = $data['totalleft'];
 
-    $stmt = $conn->prepare("INSERT INTO JoystickInput (totalup, totaldown, totalright, totalleft) VALUES (????)");
-    $stmt->bind_param("iiii", $totalup, $totaldown, $totalleft, $totalleft);
-    $stmt->execute();
-    $stmt->close();
-} else {
-    echo json_encode(["status" => "error", "message" => "nothing recieved"]);
+    $stmt = $conn->prepare("INSERT INTO JoystickInput (totalup, totaldown, totalright, totalleft) VALUES ( ?, ?, ?, ?)");
+    $stmt->bind_param("iiii", $totalup, $totalleft, $totalleft, $totalleft);
+    if ($stmt->execute()) {
+        echo json_encode([
+            "status" => "success",
+            "message" => "Data inserted",
+            "inserted_data" => [
+                "totalup"    => $totalup,
+                "totaldown"  => $totaldown,
+                "totalright" => $totalright,
+                "totalleft"  => $totalleft
+            ]
+        ]);
+    } else {
+        // Fout bij uitvoeren
+        echo json_encode([
+            "status" => "error",
+            "message" => "Execute failed: " . $stmt->error
+        ]);
+    }
 }
-
+$stmt->close();
 $conn->close();
 ?>
+
