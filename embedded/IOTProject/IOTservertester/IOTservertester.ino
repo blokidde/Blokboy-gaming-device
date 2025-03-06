255ce3322e95f8096956ca4eb43835233c8b86bb
egmondj10 <jurriaan.egmond@hva.nl>
2025-03-06 12:47:41 +0100

    testcode for webserver with wifi manager

--- embedded/IOTProject/IOTservertester/IOTservertester.ino
+++ embedded/IOTProject/IOTservertester/IOTservertester.ino
@@ -1,56 +1,56 @@
 #include <WiFiManager.h>
 #include <WiFi.h>
-#include <HTTPClient.h>
-#include <ArduinoJson.h>
-#include <WiFiClient.h>
-
-const char* ssid = "iotroam";
-const char* password = "xYEa1WO94W";
 
 WiFiServer server(80);
 
 void setup() {
-  Serial.begin(115200);
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi");
-  
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("\nConnected to WiFi!");
-  Serial.print("ESP32 IP Address: ");
-  Serial.println(WiFi.localIP());  // Print het IP-adres
-  server.begin();
+    Serial.begin(115200);
+
+    WiFiManager wifiManager;
+
+    if (!wifiManager.autoConnect("ESP32-Setup")) {
+        Serial.println("Failed to connect, restarting...");
+        delay(3000);
+        ESP.restart();
+    }
+
+    Serial.println("Connected to WiFi!");
+    Serial.print("ESP32 IP Address: ");
+    Serial.println(WiFi.localIP());
+
+    server.begin();
+    Serial.println("Webserver started");
 }
 
 void loop() {
-  WiFiClient client = server.available();
-  if (client) {
-    Serial.println("New Client connected!");
-    String request = "";
-    
-    while (client.connected()) {
-      if (client.available()) {
-        char c = client.read();
-        request += c;
-        if (c == '\n') break;
-        }
-    }
-    
-    Serial.println("Client Request: ");
-    Serial.println(request);
-    client.println("HTTP/1.1 200 OK");
-    client.println("Content-type:text/html");
-    client.println("Connection: close");
-    client.println();
-    client.println("<!DOCTYPE html><html><body>");
-    client.println("<h1>ESP32 Webserver</h1>");
-    client.println("<p>ESP32 is online!</p>");
-    client.println("</body></html>");
-    client.println();
-    client.stop();
-    Serial.println("Client disconnected.");
-  }
-}
+    WiFiClient client = server.available();
 
+    if (client) {
+        Serial.println("New Client connected!");
+        String request = "";
+
+        while (client.connected()) {
+            if (client.available()) {
+                char c = client.read();
+                request += c;
+                if (c == '\n') break;
+            }
+        }
+
+        Serial.println("Client Request: ");
+        Serial.println(request);
+
+        client.println("HTTP/1.1 200 OK");
+        client.println("Content-type:text/html");
+        client.println("Connection: close");
+        client.println();
+        client.println("<!DOCTYPE html><html><body>");
+        client.println("<h1>ESP32 Webserver met WiFiManager</h1>");
+        client.println("<p>ESP32 is verbonden met WiFi!</p>");
+        client.println("</body></html>");
+        client.println();
+
+        client.stop();
+        Serial.println("Client disconnected.");
+    }
+}
